17544893fe5bea376db21a9d600e321ebd492c48
Amanda Dittami <adittami@gmail.com>
2021-12-03 19:29:48 -0600

    improvements to UI

--- Robot-Playground/Assets/BeatEmUp_GameTemplate3D/Scripts/UI/ChooseAvatar.cs
+++ Robot-Playground/Assets/BeatEmUp_GameTemplate3D/Scripts/UI/ChooseAvatar.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseAvatar : MonoBehaviour
 {
+    public Dropdown dropdown;
+    public CustomImportNFT updatePlayerProfile;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dropdown.GetComponent<Dropdown>();
+
+        dropdown.options.Clear();
+
+        List<string> items = new List<string>();
+
+        for (int i = 1; i<55; i++)
+        {
+            items.Add(i.ToString());
+        }
+
+        foreach(string item in items)
+        {
+            dropdown.options.Add(new Dropdown.OptionData { text = item });
+        }
+
+        dropdown.onValueChanged.AddListener(delegate { DropItemSelected(dropdown); });
+    }
+
+    void DropItemSelected(Dropdown dropdown)
+    {
+        int index = dropdown.value;
+        updatePlayerProfile.tokenId = dropdown.options[index].text;
+        updatePlayerProfile.ChangeAvatar();
     }
 
     // Update is called once per frame
